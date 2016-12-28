4b6975c0bf44d36f00258979e14d86abb24c24a3
anishparajuli <anishparajuli@outlook.com>
2016-12-28 20:19:21 +0545

    commit message

--- src/gcore/Camera.java
+++ src/gcore/Camera.java
@@ -1,5 +1,12 @@
 package gcore;
 
 public class Camera extends Object {
+	int width;
+	int height;
+	
+	public void setResolution(int screenWidth,int screenHeight){
+		width=screenWidth;
+		height=screenHeight;
+	}
 
 }
