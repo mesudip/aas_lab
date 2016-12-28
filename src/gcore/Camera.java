c3934bf95e5bef2abc3203258d9684e602fc4642
anishparajuli <anishparajuli@outlook.com>
2016-12-28 20:36:23 +0545

    Camera properties update

--- src/gcore/Camera.java
+++ src/gcore/Camera.java
@@ -3,10 +3,13 @@ package gcore;
 public class Camera extends Object {
 	int width;
 	int height;
+	float fieldOfView;
 	
 	public void setResolution(int screenWidth,int screenHeight){
 		width=screenWidth;
 		height=screenHeight;
 	}
+	
+	
 
 }
